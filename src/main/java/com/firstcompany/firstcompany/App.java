c08bfd80fbe3cce81b12387317a745ddf027e3c6
maheshGontla <gontlamahesh2002@gmail.com>
2023-09-26 15:25:15 +0530

    maven project

---
+++ src/main/java/com/firstcompany/firstcompany/App.java
@@ -0,0 +1,13 @@
+package com.firstcompany.firstcompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
